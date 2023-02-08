44ef895e8a78cd6c40594b25b93f4b40d019461d
Harsha Senaratne <senaratneh@gmail.com>
2023-02-08 15:08:33 +0530

    updated User Module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
